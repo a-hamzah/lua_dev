650191741acc8824bc77b86f6cb00bee8d23a5ce
a-hamzah <Spellbreaker907@gmail.com>
2023-09-24 01:22:23 +0500

    The detailed message

--- main.lua
+++ main.lua
@@ -14,6 +14,11 @@ print(firstName .." ".. secondName)
 
 local des = [[-what are you
 -no way there
--what do you do?
-]]
+-what do you do?]]
 print(des)
+
+-- Strings
+local name1 = "John Wick"
+print(#name1) -- string length
+local name2 = "Team"
+print(name2)
