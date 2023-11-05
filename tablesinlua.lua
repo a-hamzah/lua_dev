bd8b9e644263411968c5ee49779b7d84bd0948af
a-hamzah <Spellbreaker907@gmail.com>
2023-11-05 07:20:21 +0500

    Tables

--- tablesinlua.lua
+++ tablesinlua.lua
@@ -27,3 +27,37 @@ function table.print(t)
  }
  table.sort(t, function (k1, k2) return k1.str < k2.str end )
  for i,v in pairs(t) do print(v.str,v.dex,v.wis) end
+
+ nestedTable = {
+    { x=4, label="Huey" },
+    { x=56, label="Dewey" },
+    { x=0.6, label="Louie" }
+ }
+ 
+ -- Sort it by x in increasing order
+ table.sort(nestedTable, function(a,b) return a.x < b.x end)
+ print(nestedTable[1].x .. " " .. nestedTable[2].x .. " " .. nestedTable[3].x)
+ 
+ -- Sort it by x in decreasing order
+ table.sort(nestedTable, function(a,b) return a.x > b.x end)
+ print(nestedTable[1].x .. " " .. nestedTable[2].x .. " " .. nestedTable[3].x)
+
+ 
+ -- Sort it by label in increasing order
+ table.sort(nestedTable, function(a,b) return a.label < b.label end)
+ print(nestedTable[1].label .. " " .. nestedTable[2].label .. " " .. nestedTable[3].label)
+
+ 
+ -- Sort it by label in decreasing order
+ table.sort(nestedTable, function(a,b) return a.label > b.label end)
+ print(nestedTable[1].label .. " " .. nestedTable[2].label .. " " .. nestedTable[3].label)
+
+ A = {{0,0,0}, {0,0,0}, {0,0,0}}
+ for i=1, #A, 1 do
+   for j=1, #A, 1 do
+      if i==j then
+         A[i][j] = 1 
+      end
+   end
+ end
+ table.print(A)
